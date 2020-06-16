884e20ebe69605d55db4afa7165a7c83b7e65587
joe waine <joewaine@wawa1114s-imac.local>
2020-06-16 16:55:16 -0700

    nodemod

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ mongoose
 // db configuaration ends here
 //registering cors
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
