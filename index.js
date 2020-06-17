0e507f69ff51aed1485e892380bc19303affa982
joe waine <joewaine@wawa1114s-imac.local>
2020-06-17 10:23:50 -0700

    f

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.use(morgan("dev")); // configire morgan
 // define first route
 app.get("/", (req, res) => {
 
- res.send(JSON.stringify({ Hello: 'mean solider'}));
+  // res.send(JSON.stringify(cors(){ Hello: 'mean solider'}));
+  res.send(JSON.stringify(cors()));
   console.log("Hello MEVN Soldier your update");
   console.log(cors());
 });
