9b4f2b60aa31ab1b7d3edd7f8eefd2e22a928e6a
joe waine <joewaine@wawa1114s-imac.local>
2020-06-17 12:21:31 -0700

    f

--- index.js
+++ index.js
@@ -36,7 +36,14 @@ var corsOptions = {
 }
 
 
-app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(cors(corsOptions));
 
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
