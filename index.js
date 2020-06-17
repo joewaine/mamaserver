66bccdf53998c3dba7131c232f32335f3e4f115b
joe waine <joewaine@wawa1114s-imac.local>
2020-06-16 18:13:07 -0700

    f

--- index.js
+++ index.js
@@ -34,20 +34,22 @@ mongoose
 // }
 
 
-var whitelist = ['https://sheltered-shore-34206.herokuapp.com/', 'https://determined-allen-f1662e.netlify.app/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['https://sheltered-shore-34206.herokuapp.com/', 'https://determined-allen-f1662e.netlify.app/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
  
 
-// app.use(cors(corsOptions));
-app.options('*', cors())
+let corsOptions = { origin: 'http://localhost:8080/' }
+
+app.use(cors(corsOptions));
+// app.options('*', cors())
 
 
 //configure body parser
