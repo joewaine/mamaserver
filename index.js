316041c40f97eb84ddb662ae473460681b943e80
joe waine <joewaine@wawa1114s-imac.local>
2020-06-17 10:15:37 -0700

    f

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
