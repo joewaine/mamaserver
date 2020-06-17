9bb9db05f69cc42a937cb0de5a572123c8d3557f
joe waine <joewaine@wawa1114s-imac.local>
2020-06-16 18:18:28 -0700

    f

--- index.js
+++ index.js
@@ -48,8 +48,15 @@ mongoose
 
 let corsOptions = { origin: '*' }
 
-app.use(cors(corsOptions));
-// app.options('*', cors())
+// app.use(cors(corsOptions));
+// // app.options('*', cors())
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //configure body parser
