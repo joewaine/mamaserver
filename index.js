198495ff0487b807412a6161d736cfb9e169be92
joe waine <joewaine@wawa1114s-imac.local>
2020-06-16 18:19:24 -0700

    f

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ let corsOptions = { origin: '*' }
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
