dbeb94cc2375b2b6f109a1b715cc5b22c5569d37
joe waine <joe.waine@gmail.com>
2020-06-23 20:08:19 -0700

    f

--- api/user/route/user.js
+++ api/user/route/user.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../../config/auth");
 const userController = require("../controller/userController");
+const cors = require("cors");
+
+
+let corsOptions = { origin: true }
+
 
 router.post("/register", userController.registerNewUser);
-router.post("/login", userController.loginUser);
+router.post("/login", cors(corsOptions), userController.loginUser);
 router.post("/addproduct", userController.addProduct);
 router.get('/:id', userController.getUserProducts);
       
